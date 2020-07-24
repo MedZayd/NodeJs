676d55350e97b456dfb54499c3b2295fba2f9d34
Mohamed Zayd <abadmohamedzayd@gmail.com>
2020-07-24 21:28:30 +0200

    parsing request body and storing in file.txt

--- ch3/app.js
+++ ch3/app.js
@@ -1,4 +1,5 @@
-const http = require("http");
+const http = require('http');
+const fs = require('fs');
 
 /**
  * createServer takes as argument a requestListener function
@@ -11,18 +12,51 @@ const http = require("http");
  *  res: ServerResponse
  */
 const server = http.createServer((req, res) => {
-  console.log({ url: req.url, method: req.method, headers: req.headers });
-
-  /**
-   * Hard exit the event loop server listener
-   */
-  // process.exit();
-
-  res.setHeader("Content-type", "text/html");
-  res.write("<html>");
-  res.write("<header><title>My first page with Node JS</title></header>");
-  res.write("<body><h1>Hello Owls</h1></body>");
-  res.write("</html>");
+  const { url, method } = req;
+  console.log({ url, method });
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head>');
+    res.write('<title>My first page with Node JS</title>');
+    res.write(
+      '<link rel="icon" href="https://nodejs.org/static/images/favicons/favicon.ico">'
+    );
+    res.write('</head>');
+    res.write('<body>');
+    res.write('<form action="/message" method="POST">');
+    res.write('<input type="text" name="message">');
+    res.write('<button type="submit">Send</button>');
+    res.write('</form>');
+    res.write('</body>');
+    res.write('</html>');
+    return res.end();
+  }
+  if (url === '/message' && method === 'POST') {
+    // Parsing our data
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log({ chunk });
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('message.txt', message);
+    });
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
+  res.setHeader('Content-type', 'text/html');
+  res.write('<html>');
+  res.write('<head>');
+  res.write('<title>My first page with Node JS</title>');
+  res.write(
+    '<link rel="icon" href="https://nodejs.org/static/images/favicons/favicon.ico">'
+  );
+  res.write('</head>');
+  res.write('<body><h1>Hello Owls</h1></body>');
+  res.write('</html>');
   res.end();
 });
 
