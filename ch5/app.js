a1ae030a87fd45d960e4cff7277d97631d0dab1c
Mohamed Zayd <abadmohamedzayd@gmail.com>
2020-07-25 15:53:32 +0200

    express js middlewares

--- ch5/app.js
+++ ch5/app.js
@@ -4,6 +4,26 @@ const express = require('express');
 
 const app = express();
 
+/**
+ * @argument function executes for every req
+ */
+app.use(
+  /**
+   * @argument req request
+   * @argument res response
+   * @argument next function
+   */
+  (req, res, next) => {
+    console.log('In the middleware');
+    next();
+  }
+);
+
+app.use((req, res, next) => {
+  console.log('In another middleware');
+  res.send('<h>Hello from Express JS</h>');
+});
+
 const server = http.createServer(app);
 
 server.listen(3000);
