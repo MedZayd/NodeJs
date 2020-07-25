de89ea557f04e253853048bdf0207dfa1c525c1d
Mohamed Zayd <abadmohamedzayd@gmail.com>
2020-07-25 23:10:56 +0200

    dynamci rendering for products list

--- ch6/routes/shop.js
+++ ch6/routes/shop.js
@@ -6,7 +6,8 @@ const adminData = require('./admin');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-	res.render('shop');
+	const { products } = adminData;
+	res.render('shop', { products, pageTitle: 'My Shopify' });
 });
 
 module.exports = router;
