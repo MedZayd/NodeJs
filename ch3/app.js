f67f4886c90320dc1a2557a78d3be4d685d912f3
Mohamed Zayd <abadmohamedzayd@gmail.com>
2020-07-25 13:30:27 +0200

    node js basic

--- ch3/app.js
+++ ch3/app.js
@@ -1,5 +1,5 @@
 const http = require('http');
-const fs = require('fs');
+const routes = require('./routes');
 
 /**
  * createServer takes as argument a requestListener function
@@ -11,53 +11,6 @@ const fs = require('fs');
  *  req: IncomingMessage,
  *  res: ServerResponse
  */
-const server = http.createServer((req, res) => {
-  const { url, method } = req;
-  console.log({ url, method });
-  if (url === '/') {
-    res.write('<html>');
-    res.write('<head>');
-    res.write('<title>My first page with Node JS</title>');
-    res.write(
-      '<link rel="icon" href="https://nodejs.org/static/images/favicons/favicon.ico">'
-    );
-    res.write('</head>');
-    res.write('<body>');
-    res.write('<form action="/message" method="POST">');
-    res.write('<input type="text" name="message">');
-    res.write('<button type="submit">Send</button>');
-    res.write('</form>');
-    res.write('</body>');
-    res.write('</html>');
-    return res.end();
-  }
-  if (url === '/message' && method === 'POST') {
-    // Parsing our data
-    const body = [];
-    req.on('data', (chunk) => {
-      console.log({ chunk });
-      body.push(chunk);
-    });
-    req.on('end', () => {
-      const parsedBody = Buffer.concat(body).toString();
-      const message = parsedBody.split('=')[1];
-      fs.writeFileSync('message.txt', message);
-    });
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
-  }
-  res.setHeader('Content-type', 'text/html');
-  res.write('<html>');
-  res.write('<head>');
-  res.write('<title>My first page with Node JS</title>');
-  res.write(
-    '<link rel="icon" href="https://nodejs.org/static/images/favicons/favicon.ico">'
-  );
-  res.write('</head>');
-  res.write('<body><h1>Hello Owls</h1></body>');
-  res.write('</html>');
-  res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
